9690033bc24fc93821502f44ceca3ffb5dbba33f
Chancy <chancyleath@hotmail.com>
2018-08-23 11:59:09 -0600

    initial files created

---
+++ javascript/app.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+    //declare global variables
+
+    //object array of all questions and which id is the correct answer?
+    var questions = {
+        question1: {
+            name: "Q1",
+            question: "What is Cher's real birth name?",
+            answer: "Cherilyn Sarkisian",
+            idAnswer: "#q1a1"
+        },
+    }
+
+    // correctAnswerChosen = false;
+    //answerChosen = [];
+
+
+
+
+
+
+
+
+
+
+
+
+
+//functions=======================
+
+//a start function to clear all previous answer and start the countdown when start button is selected
+
+//do a form timer include a countdown in the dom
+
+//verify the correct answer selected
+//move selected answer id into the answerchosen variable (empty array or variable)
+    //conditional if answerChosen ==== id of correct answer
+        //then add to correct tally
+    //if else, left blank or !== to id of correct answer
+        //then move to incorrect tally
+
+
+//total the number correct, incorrect
+
+
+
+
+//processes and main game play ========================================
+
+
+
+
+
+
+
+
+})
