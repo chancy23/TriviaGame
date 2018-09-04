17e2fc54932a7e20dcb25948610e1a4b20d70d46
Chancy <chancyleath@hotmail.com>
2018-09-04 14:46:39 -0600

    fixed correct answers function to work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
     //object array of all questions, answers, and the correct answer to each question, also a class to assign  to each question and answer set
     var questions = [{
@@ -6,40 +6,35 @@ $(document).ready(function(){
             question: "What is Cher's real birth name?",
             answers: ["Cherilyn Sarkisian", "Cher Saskatoon", "Cheryl Sanford", "Cheryl Ann Sarswesian"],
             correctAnswer: "Cherilyn Sarkisian",
-            divClass: ".birthName"
         },
         {
             name: "Q2",
             question: "What award has Cher not won?",
             answers: ["A Grammy", "An Oscar", "An Emmy", "A Tony"],
             correctAnswer: "A Tony",
-            divClass: ".awards"
         },
         {
             name: "Q3",
             question: "What 1998 hit pioneered the use of Auto-Tune?",
             answers: ["Strong Enough", "Believe", "The Power", "When the Money's Gone"],
             correctAnswer: "Believe",
-            divClass: ".autoTune"
         },
         {
             name: "Q4",
             question: "What movie was Cher's film debut in?",
             answers: ["Mask", "Moonstruck", "Silkwood", "Tea with Mussolini"],
             correctAnswer: "Silkwood",
-            divClass: ".filmDebut"
         },
         {
             name: "Q5",
             question: "What band is Cher covering on her upcoming album 'Dancing Queen'?",
             answers: ["Journey", "ABBA", "REO Speedwagon", "Fleetwood Mac"],
             correctAnswer: "ABBA",
-            divClass: ".coverAlbum"
         }
     ];
 
-    //displays the out of total questions in results div
-    $("#totalNumQues").text(questions.length);
+    var numCorrect = 0;
+    var numWrong = 0;
 
     //do a form timer include a countdown in browser
     var intervalId;
@@ -75,81 +70,85 @@ $(document).ready(function(){
             //when timer stops, check the answers and display results of number correct.
             checkAnswer();
             $(".displayResults").show();
+            $(".allQuestions").hide();
             }
         }
     };
-    
-    //var correctAnswerChosen = false;
-    var numCorrect = 0;
-    var numWrong = 0;
+
     //functions=======================================================
 
     //resets the game at start and if Try Again button is pushed.
-    //clears all divs, resets results, resets timer to start at 60 seconds
+    //clears all divs, resets results, resets timer to start at 30 seconds
     function resetGame(){
         timer.reset();
+        numCorrect = 0;
+        numWrong = 0;
         $("#start").show();
-        $(".allQuestions").empty();
-        $("#results").empty();
+        $(".allQuestions, #results").empty();
         $(".displayResults").hide();
     };
 
     //loads questions and their corresponding answers to the browser
     function loadQuestions() {
-        
+        $(".allQuestions").show();
         // loops through the 5 questions and loads to div, assigns a class for each question.
         for (var i = 0; i < questions.length; i++) {
-        var questionTitle = $("<div>").addClass(questions[i].divClass).addClass("titles").text(questions[i].question);
-        $(".allQuestions").append(questionTitle);
-
-            //loops through each answer from object array and assigns a radio button and adds to each questions
-            for (var j = 0; j < 4; j++) {
-                 var answer = $("<div>").addClass("allAnswers").text(questions[i].answers[j]);
-                $(answer).prepend($('<input type="radio" class="answerButton" value="' + questions[i].answers[j] + '" name="' + questions[i].name +'">' + questions[i].answers[j]));
-                $(questionTitle).append(answer);
-            }
-        }
+            $(".allQuestions").append("<h5>" + questions[i].question + "</h5>").addClass("questionTitle");
+                //loops through each answer from object array and assigns a radio button and adds to each questions
+                for (var j = 0; j < questions[i].answers.length; j++) {
+                    $(".allQuestions").append("<input type='radio' name='question-" + i + "' value='" + questions[i].answers[j] + "'>" + questions[i].answers[j]);
+                };
+        };
    };
 
    function checkAnswer() {
+   
+        $.each($("input[name='question-0']:checked"), function() {
+            if ($(this).val() === questions[0].correctAnswer){
+                numCorrect++;
+            }
+            else {
+                numWrong++;
+            };
+        });
+        $.each($("input[name='question-1']:checked"), function() {
+            if ($(this).val() === questions[1].correctAnswer){
+                numCorrect++;
+            }
+            else {
+                numWrong++;
+            };
+        });
+        $.each($("input[name='question-2']:checked"), function() {
+            if ($(this).val() === questions[2].correctAnswer){
+                numCorrect++;
+            }
+            else {
+                numWrong++;
+            };
+        });
+        $.each($("input[name='question-3']:checked"), function() {
+            if ($(this).val() === questions[3].correctAnswer){
+                numCorrect++;
+            }
+            else {
+                numWrong++;
+            };
+        });
+        $.each($("input[name='question-4']:checked"), function() {
+            if ($(this).val() === questions[4].correctAnswer){
+                numCorrect++;
+            }
+            else {
+                numWrong++;
+            };
+        });
 
-    // variable to run check answer as long as right answer isn't chosen (from global variable)
-    //correctAnswerChosen;
-    
-    // gather selected answers from document, and loop through each and add the value
-    var selectedAnswers = document.querySelectorAll("input[type=radio]:checked");
-        for (var l = 0; l < selectedAnswers.length; l++) {
-            //add the trim function to eliminate white space so would match right answers, didn't work
-            selectedAnswers[l].value.trim();
-            console.log("this is the selected answer: " + selectedAnswers[l].value);
-        };
-
-        //loop through the questions object to get the correct answer for each question
-        //add the trim function to eliminate white space so would match selected answers, didn't work
-        for (var k = 0; k < questions.length; k++) {
-            var rightAnswer = questions[k].correctAnswer.trim();
-            console.log("this is the right answer: " + rightAnswer);
-            
-        };
-        //****Note: this isn't working, but should check the selected answer against the correct answer and return either true or false
-        //check the selected answer matches the right answer
-        if (selectedAnswers[l] === rightAnswer[k]){
-            // add to the correct count
-            numCorrect++
-            //correctAnswerChosen = true;
-            $("#results").append(numCorrect);
-            console.log("this is the number correct: " + numCorrect);
-        }
-        // if answer is wrong or blank
-        else {
-            //add to wrong count
-            numWrong++;
-            //not longer need a wrong number count to display
-            //$("#results").append("Number of Incorrect Answers: " + numWrong);
-            console.log("this is the number wrong: " + numWrong);
-        };
-        
+        $("#results").append("<h6>Number Correct: " + numCorrect + "</h6>");
+        $("#results").append("<h6>Number Wrong: " + numWrong + "</h6>");
+        $("#results").append("<h6>Number Unanswered: " + (questions.length - (numWrong + numCorrect)) + "</h6>");
     };
+     
     
     
     //processes and main game play =======================================
